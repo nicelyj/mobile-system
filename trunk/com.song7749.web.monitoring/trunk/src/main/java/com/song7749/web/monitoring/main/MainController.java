91d0a15ec2495924f0c7d140d836a20b8ce08091
song7749@gmail.com <song7749@gmail.com@a94cb3e7-1f80-c8c3-d04d-52d12b8a197f>
2011-08-18 10:11:38 +0000

    기본컨트롤러 구현 완료

--- trunk/com.song7749.web.monitoring/trunk/src/main/java/com/song7749/web/monitoring/main/MainController.java
+++ trunk/com.song7749.web.monitoring/trunk/src/main/java/com/song7749/web/monitoring/main/MainController.java
@@ -1,16 +1,20 @@
 package com.song7749.web.monitoring.main;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	@ResponseBody
-	public String hello(@RequestParam String name) {
-		return "Hello " + name;
+	@RequestMapping("/index.html")
+	public String mainIndex(HttpServletRequest request,
+			HttpServletResponse response, ModelMap modelMap) {
+
+		String viewTemplete = "main/index";
+
+		return viewTemplete;
 	}
 }
