cb74b1c5c00754fed27c9a44c6e875d521d93b16
song7749@gmail.com <song7749@gmail.com@a94cb3e7-1f80-c8c3-d04d-52d12b8a197f>
2011-09-27 02:18:03 +0000

    database state/process

--- trunk/com.song7749.core/trunk/src/main/java/com/song7749/util/DatabaseStateUtil.java
+++ trunk/com.song7749.core/trunk/src/main/java/com/song7749/util/DatabaseStateUtil.java
@@ -5,19 +5,26 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import org.apache.log4j.Logger;
 import org.springframework.orm.ibatis.SqlMapClientTemplate;
 
+import com.ibatis.sqlmap.client.SqlMapSession;
 import com.song7749.util.database.model.DatabaseProcessForMysql;
 import com.song7749.util.database.model.DatabaseStateForMysql;
 
 public class DatabaseStateUtil {
+	private static Logger logger = Logger.getLogger(DatabaseStateUtil.class);
+
 	public static ArrayList<DatabaseStateForMysql> getMysqlDatabaseState(
 			SqlMapClientTemplate dataSource) throws SQLException {
 		String sql = "show status";
 		PreparedStatement statement = null;
 		ResultSet rs = null;
+		SqlMapSession session = dataSource.getSqlMapClient().openSession();
 		try {
-			statement = dataSource.getSqlMapClient().getDataSource().getConnection().prepareStatement(sql);
+
+			statement = session.getDataSource().getConnection()
+					.prepareStatement(sql);
 			rs = statement.executeQuery();
 			ArrayList<DatabaseStateForMysql> stateList = new ArrayList<DatabaseStateForMysql>();
 			while (rs.next()) {
@@ -29,10 +36,11 @@ public class DatabaseStateUtil {
 			return stateList;
 		} catch (SQLException e) {
 			throw e;
-		} finally{
+		} finally {
 			rs.close();
+			statement.getConnection().close();
 			statement.close();
-			dataSource.getSqlMapClient().getDataSource().getConnection().close();
+			session.close();
 		}
 	}
 
@@ -41,13 +49,14 @@ public class DatabaseStateUtil {
 		String sql = "show processlist";
 		PreparedStatement statement = null;
 		ResultSet rs = null;
+		SqlMapSession session = dataSource.getSqlMapClient().openSession();
 		try {
-			statement = dataSource.getSqlMapClient().getDataSource().getConnection().prepareStatement(sql);
+			statement = session.getDataSource().getConnection()
+					.prepareStatement(sql);
 			rs = statement.executeQuery();
 			ArrayList<DatabaseProcessForMysql> stateList = new ArrayList<DatabaseProcessForMysql>();
 			while (rs.next()) {
 				DatabaseProcessForMysql databaseProcessForMysql = new DatabaseProcessForMysql();
-
 				databaseProcessForMysql.setCommand(rs.getString("Command"));
 				databaseProcessForMysql.setDb(rs.getString("db"));
 				databaseProcessForMysql.setHost(rs.getString("Host"));
@@ -62,10 +71,11 @@ public class DatabaseStateUtil {
 			return stateList;
 		} catch (SQLException e) {
 			throw e;
-		} finally{
+		} finally {
 			rs.close();
+			statement.getConnection().close();
 			statement.close();
-			dataSource.getSqlMapClient().getDataSource().getConnection().close();			
+			session.close();
 		}
 	}
 }
