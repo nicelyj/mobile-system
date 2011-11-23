5c189a62293ec96499937bc425baa839eb153cf5
song7749@gmail.com <song7749@gmail.com@a94cb3e7-1f80-c8c3-d04d-52d12b8a197f>
2011-11-23 05:57:32 +0000

    캐릭터셋 변경

--- trunk/com.song7749.core/trunk/src/main/java/com/song7749/base/SqlMapClientFactoryBeanDynamic.java
+++ trunk/com.song7749.core/trunk/src/main/java/com/song7749/base/SqlMapClientFactoryBeanDynamic.java
@@ -9,9 +9,35 @@ import org.springframework.core.io.Resource;
 import org.springframework.orm.ibatis.SqlMapClientFactoryBean;
 import org.springframework.util.Log4jConfigurer;
 
+/**
+ * <pre>
+ * Class Name : SqlMapClientFactoryBeanDynamic.java
+ * Description : 
+ * 
+ *  Modification Information
+ *  Modify Date 			Modifier			Comment
+ * -----------------------------------------------
+ *  2011. 11. 23.		song7749		    신규 생성
+ * 
+ * </pre>
+ * 
+ * @author song7749
+ * @since 2011. 11. 23.
+ */
 public class SqlMapClientFactoryBeanDynamic extends SqlMapClientFactoryBean {
 	private static boolean isPrinted = false;
 
+	/**
+	 * 환경에 따라 넘겨받은 파라메터 중 하나를 선택하여 작동. 환경구분을 위해 시스템 환경변수를 사용함. 개발서버(테스트서버)에는 반드시
+	 * JAVA_SERVER_SORT=TEST 가 설정되어야 함.
+	 * 
+	 * @param developer
+	 *            개발자 PC의 설정.
+	 * @param testServer
+	 *            개발서버(테스트서버)의 설정.
+	 * @param realServer
+	 *            실제 서비스 서버의 설정.
+	 */
 	public SqlMapClientFactoryBeanDynamic(String developer, String testServer,
 			String realServer) {
 		Log logger = LogFactory.getLog(Log4jConfigurer.class);
